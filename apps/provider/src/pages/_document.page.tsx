268803b77c7a717a9020ed23e70f45489bc9509b
Benjamin Bender <bb@codepoet.de>
2021-12-20 15:19:41 +0100

    Fix CSP

--- apps/provider/src/pages/_document.page.tsx
+++ apps/provider/src/pages/_document.page.tsx
@@ -1,29 +1,19 @@
-import crypto from "crypto";
 import NextDocument, { Head, Html, Main, NextScript } from "next/document";
 
-const cspHashOf = (text: string) => {
-  const hash = crypto.createHash("sha256");
-  hash.update(text);
-
-  return `'sha256-${hash.digest("base64")}'`;
-};
-
+/*
+ * Note on CSP: we are blocking everything and allow only the bits we need from "self"
+ */
 class Document extends NextDocument {
+  protected csp =
+    process.env.NODE_ENV !== "production"
+      ? "style-src 'self' 'unsafe-inline'; font-src 'self' data:; default-src 'self'; script-src 'unsafe-eval' 'self'"
+      : "default-src 'none'; style-src 'self'; manifest-src 'self'; connect-src 'self'; script-src 'self'; font-src 'self'; img-src 'self' data:; prefetch-src 'self'";
+
   render() {
-    let csp = `default-src 'self'; script-src 'self' ${cspHashOf(
-      NextScript.getInlineScriptSource(this.props)
-    )}`;
-
-    if (process.env.NODE_ENV !== "production") {
-      csp = `style-src 'self' 'unsafe-inline'; font-src 'self' data:; default-src 'self'; script-src 'unsafe-eval' 'self' ${cspHashOf(
-        NextScript.getInlineScriptSource(this.props)
-      )}`;
-    }
-
     return (
-      <Html>
+      <Html dir="ltr">
         <Head>
-          <meta httpEquiv="Content-Security-Policy" content={csp} />
+          <meta httpEquiv="Content-Security-Policy" content={this.csp} />
         </Head>
         <body>
           <Main />
--- apps/user/src/pages/_document.page.tsx
+++ apps/user/src/pages/_document.page.tsx
@@ -7,13 +7,13 @@ class Document extends NextDocument {
   protected csp =
     process.env.NODE_ENV !== "production"
       ? "style-src 'self' 'unsafe-inline'; font-src 'self' data:; default-src 'self'; script-src 'unsafe-eval' 'self'"
-      : "default-src 'none'; style-src 'self'; manifest-src 'self'; script-src 'self'; font-src 'self'; img-src 'self' data:; prefetch-src 'self'";
+      : "default-src 'none'; style-src 'self'; manifest-src 'self'; connect-src 'self'; script-src 'self'; font-src 'self'; img-src 'self' data:; prefetch-src 'self'";
 
   render() {
     return (
       <Html dir="ltr">
         <Head>
-          {/* <meta httpEquiv="Content-Security-Policy" content={this.csp} /> */}
+          <meta httpEquiv="Content-Security-Policy" content={this.csp} />
         </Head>
         <body>
           <Main />
