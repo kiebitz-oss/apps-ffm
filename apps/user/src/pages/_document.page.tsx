b8218fb00d3538e61edcd30825e195cf580e21de
Benjamin Bender <bb@codepoet.de>
2022-01-31 18:59:49 +0100

    fix: deployment requirements #2

--- apps/user/src/pages/_document.page.tsx
+++ apps/user/src/pages/_document.page.tsx
@@ -6,8 +6,24 @@ import NextDocument, { Head, Html, Main, NextScript } from "next/document";
 class Document extends NextDocument {
   protected csp =
     process.env.NODE_ENV !== "production"
-      ? `default-src 'none'; prefetch-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self' data:; script-src 'unsafe-eval' 'self'; connect-src 'self' ws://localhost:3000/_next/webpack-hmr  ${process.env.NEXT_PUBLIC_APPOINTMENTS_ENDPOINT} ${process.env.NEXT_PUBLIC_STORAGE_ENDPOINT}; img-src 'self' data:;`
-      : `default-src 'none'; prefetch-src 'self'; style-src 'self'; manifest-src 'self'; connect-src 'self' ${process.env.NEXT_PUBLIC_APPOINTMENTS_ENDPOINT} ${process.env.NEXT_PUBLIC_STORAGE_ENDPOINT}; script-src 'self'; font-src 'self'; img-src 'self' data:;`;
+      ? `default-src 'none'; prefetch-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self' data:; script-src 'unsafe-eval' 'self'; connect-src 'self' ws://localhost:3000/_next/webpack-hmr${
+          process.env.NEXT_PUBLIC_APPOINTMENTS_ENDPOINT
+            ? new URL(process.env.NEXT_PUBLIC_APPOINTMENTS_ENDPOINT).origin
+            : ""
+        }${
+          process.env.NEXT_PUBLIC_STORAGE_ENDPOINT
+            ? new URL(process.env.NEXT_PUBLIC_STORAGE_ENDPOINT).origin
+            : ""
+        }; img-src 'self' data:;`
+      : `default-src 'none'; prefetch-src 'self'; style-src 'self'; manifest-src 'self'; connect-src 'self'${
+          process.env.NEXT_PUBLIC_APPOINTMENTS_ENDPOINT
+            ? new URL(process.env.NEXT_PUBLIC_APPOINTMENTS_ENDPOINT).origin
+            : ""
+        }${
+          process.env.NEXT_PUBLIC_STORAGE_ENDPOINT
+            ? new URL(process.env.NEXT_PUBLIC_STORAGE_ENDPOINT).origin
+            : ""
+        }; script-src 'self'; font-src 'self'; img-src 'self' data:;`;
 
   render() {
     return (
--- packages/common/src/utils/apiConfig.ts
+++ packages/common/src/utils/apiConfig.ts
@@ -3,10 +3,10 @@ export const getApiConfig = () => {
     jsonrpc: {
       appointments: process.env.NEXT_PUBLIC_APPOINTMENTS_ENDPOINT
         ? process.env.NEXT_PUBLIC_APPOINTMENTS_ENDPOINT
-        : `${window.origin}/api/v1/appointments/jsonrpc`,
+        : `${window.origin}/api/v1/appointments`,
       storage: process.env.NEXT_PUBLIC_STORAGE_ENDPOINT
         ? process.env.NEXT_PUBLIC_STORAGE_ENDPOINT
-        : `${window.origin}/api/v1/storage/jsonrpc`,
+        : `${window.origin}/api/v1/storage`,
     },
   };
 };
