545ce556a837eb1d0afe8753cb345f9eb83f0dd1
Benjamin Bender <bb@codepoet.de>
2022-01-31 19:27:05 +0100

    fix: deployment requirements #4

--- apps/user/src/pages/_document.page.tsx
+++ apps/user/src/pages/_document.page.tsx
@@ -8,20 +8,22 @@ class Document extends NextDocument {
     process.env.NODE_ENV !== "production"
       ? `default-src 'none'; prefetch-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self' data:; script-src 'unsafe-eval' 'self'; connect-src 'self' ws://localhost:3000/_next/webpack-hmr${
           process.env.NEXT_PUBLIC_APPOINTMENTS_ENDPOINT
-            ? new URL(process.env.NEXT_PUBLIC_APPOINTMENTS_ENDPOINT).origin
+            ? " " +
+              new URL(process.env.NEXT_PUBLIC_APPOINTMENTS_ENDPOINT).origin
             : ""
         }${
           process.env.NEXT_PUBLIC_STORAGE_ENDPOINT
-            ? new URL(process.env.NEXT_PUBLIC_STORAGE_ENDPOINT).origin
+            ? " " + new URL(process.env.NEXT_PUBLIC_STORAGE_ENDPOINT).origin
             : ""
         }; img-src 'self' data:;`
       : `default-src 'none'; prefetch-src 'self'; style-src 'self'; manifest-src 'self'; connect-src 'self'${
           process.env.NEXT_PUBLIC_APPOINTMENTS_ENDPOINT
-            ? new URL(process.env.NEXT_PUBLIC_APPOINTMENTS_ENDPOINT).origin
+            ? " " +
+              new URL(process.env.NEXT_PUBLIC_APPOINTMENTS_ENDPOINT).origin
             : ""
         }${
           process.env.NEXT_PUBLIC_STORAGE_ENDPOINT
-            ? new URL(process.env.NEXT_PUBLIC_STORAGE_ENDPOINT).origin
+            ? " " + new URL(process.env.NEXT_PUBLIC_STORAGE_ENDPOINT).origin
             : ""
         }; script-src 'self'; font-src 'self'; img-src 'self' data:;`;
 
