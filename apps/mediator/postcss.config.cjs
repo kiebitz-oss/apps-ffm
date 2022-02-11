5f75638141d38f204f3babf9320fedc520ec5bef
Benjamin Bender <bb@codepoet.de>
2022-02-11 14:02:40 +0100

    fix: try to fix postcss

--- apps/mediator/postcss.config.cjs
+++ apps/mediator/postcss.config.cjs
@@ -1,3 +1,16 @@
-const config = require("@impfen/postcss-config");
-
-module.exports = config;
+module.exports = {
+  plugins: {
+    "postcss-import": {},
+    "postcss-preset-env": {
+      debug: true,
+      stage: 3,
+      autoprefixer: {
+        flexbox: "no-2009",
+      },
+      features: {
+        "nesting-rules": true,
+        "custom-media-queries": true,
+      },
+    },
+  },
+};
