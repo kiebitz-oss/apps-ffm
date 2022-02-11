c622bccddd1a67b0a5bb10eaf2cdefa61426f25e
Benjamin Bender <bb@codepoet.de>
2022-02-11 12:18:42 +0100

    feat: switch to demo-theme and change to inoeg-green

--- packages/common/src/theme/index.ts
+++ packages/common/src/theme/index.ts
@@ -1,12 +1,7 @@
-// import { demoTheme } from "./demo";
-import { ffmTheme } from "./ffm";
+import { demoTheme } from "./demo";
+// import { ffmTheme } from "./ffm";
 import type { Theme } from "./Theme";
 
 export type { Theme } from "./Theme";
 
-// export const themes: Record<string, Theme> = {
-//   demo: demoTheme,
-//   ffm: ffmTheme,
-// };
-
-export const theme: Theme = ffmTheme;
+export const theme: Theme = demoTheme;
