c32120b4fdfb3b571dd7e73c5223d5e480ee0401
Benjamin Bender <bb@codepoet.de>
2022-02-11 14:33:26 +0100

    fix: finally? postcss works as it should?

--- apps/provider/postcss.config.cjs
+++ apps/provider/postcss.config.cjs
@@ -1,16 +1,3 @@
-module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "postcss-preset-env": {
-      // debug: true,
-      stage: 3,
-      autoprefixer: {
-        flexbox: "no-2009",
-      },
-      features: {
-        "nesting-rules": true,
-        "custom-media-queries": true,
-      },
-    },
-  },
-};
+const config = require("@impfen/postcss-config");
+
+module.exports = config;
--- packages/common/src/components/Dialog.svelte
+++ packages/common/src/components/Dialog.svelte
@@ -80,10 +80,5 @@
     &:hover {
       color: var(--primary);
     }
-
-    & svg {
-      width: 100%;
-      height: 100%;
-    }
   }
 </style>
