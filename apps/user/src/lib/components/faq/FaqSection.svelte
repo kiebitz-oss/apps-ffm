692cc609b8af0cfa07770b8632d043ec59bfb7c9
Benjamin Bender <bb@codepoet.de>
2022-04-06 16:15:54 +0200

    update faq + styling

--- apps/user/src/lib/components/faq/FaqSection.svelte
+++ apps/user/src/lib/components/faq/FaqSection.svelte
@@ -2,7 +2,7 @@
   export let title: string;
 </script>
 
-<section {...$$restProps}>
+<section class="faq-section" {...$$restProps}>
   <h2 class="h3">
     {title}
   </h2>
@@ -13,18 +13,45 @@
 </section>
 
 <style lange="postcss">
-  section {
+  .faq-section {
     display: flex;
     flex-direction: column;
     gap: 2rem;
     margin-bottom: 3rem;
   }
 
-  h2 {
+  h2,
+  .faq-section :global(h3),
+  .faq-section :global(h4) {
     font-weight: 500;
     font-size: 1.25rem;
     letter-spacing: 0.05em;
     text-transform: uppercase;
+  }
+
+  .faq-section :global(h3) {
+    margin-top: 2rem;
+    font-size: 1.125rem;
+  }
+
+  .faq-section :global(h4) {
+    font-size: 1rem;
+  }
+
+  .faq-section :global(h3),
+  .faq-section :global(h4),
+  .faq-section :global(p) {
+    margin-bottom: 1rem;
+  }
+
+  .faq-section :global(p) {
+    font-size: 1rem;
+    margin-top: 0.5rem;
+  }
+
+  .faq-section :global(ul) {
+    list-style-position: inside;
+    margin-bottom: 1.5rem;
   }
 
   div {
